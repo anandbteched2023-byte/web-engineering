3630842db5e804785e72c8e611398747a5e60b92
Anand Kumar Jaiswal <anand_bteched2023@kusoed.edu.np@example.com>
2026-02-01 08:38:57 +0545

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+// import "./app.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from "./pages/login";
+import Signup from "./pages/signup";
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
